6f3f38c42d285676c4a04a4af617d80f585ed93f
mbarley333 <jacksonkato808@gmail.com>
2022-01-24 16:33:02 -1000

    insert

--- linked_list.go
+++ linked_list.go
@@ -13,10 +13,70 @@ type LinkedList struct {
 func (l LinkedList) Read(index int) string {
 	current_node := l.List
 	current_index := 0
+	value := ""
 
-	for current_index < index {
+	for current_index < index && current_node.Next != nil {
 		current_node = current_node.Next
 		current_index += 1
+		value = current_node.Data
+
 	}
-	return current_node.Data
+
+	if index > current_index {
+		value = "NotFound"
+	}
+	return value
+
+}
+
+func (l LinkedList) Search(value string) int {
+
+	current_node := l.List
+	current_index := 0
+	isFound := false
+	index := -1
+
+	for !isFound {
+
+		if current_node.Next == nil {
+			break
+		}
+
+		if current_node.Data == value {
+			isFound = true
+			index = current_index
+		}
+
+		current_index += 1
+		current_node = current_node.Next
+
+	}
+
+	return index
+}
+
+func (l *LinkedList) Insert(index int, data string) error {
+	newNode := Node{
+		Data: data,
+	}
+
+	if index == 0 {
+		newNode.Next = l.List
+		l.List = &newNode
+		return nil
+	}
+
+	current_node := l.List
+	current_index := 0
+
+	for current_index < index-2 {
+		current_node = current_node.Next
+		current_index += 1
+
+	}
+
+	newNode.Next = current_node.Next
+	current_node.Next = &newNode
+
+	return nil
 }
--- linked_list_test.go
+++ linked_list_test.go
@@ -3,6 +3,8 @@ package algo_test
 import (
 	"algo"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestLinkedList(t *testing.T) {
@@ -37,6 +39,84 @@ func TestLinkedList(t *testing.T) {
 
 	if want != got {
 		t.Fatalf("want:%q, got:%q", want, got)
+	}
+
+}
+
+func TestLinkedListSearch(t *testing.T) {
+	t.Parallel()
+
+	n1 := &algo.Node{
+		Data: "once",
+	}
+
+	n2 := &algo.Node{
+		Data: "upon",
+	}
+
+	n3 := &algo.Node{
+		Data: "a",
+	}
+
+	n4 := &algo.Node{
+		Data: "time",
+	}
+
+	n1.Next = n2
+	n2.Next = n3
+	n3.Next = n4
+
+	l := algo.LinkedList{
+		List: n1,
+	}
+
+	want := 0
+	got := l.Search("time")
+
+	if want != got {
+		t.Fatalf("want:%d, got:%d", want, got)
+	}
+
+}
+
+func TestLinkedListInsert(t *testing.T) {
+	t.Parallel()
+
+	l := algo.LinkedList{
+		List: &algo.Node{
+			Data: "yellow",
+			Next: &algo.Node{
+				Data: "blue",
+				Next: &algo.Node{
+					Data: "green",
+					Next: &algo.Node{
+						Data: "red",
+						Next: nil}}},
+		},
+	}
+
+	//want := algo.LinkedList{}
+	want := algo.LinkedList{
+		List: &algo.Node{
+			Data: "yellow",
+			Next: &algo.Node{
+				Data: "blue",
+				Next: &algo.Node{
+					Data: "black",
+					Next: &algo.Node{
+						Data: "green",
+						Next: &algo.Node{
+							Data: "red",
+							Next: nil}}}},
+		},
+	}
+
+	l.Insert(3, "black")
+
+	got := l
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 
 }
